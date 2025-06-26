b2cd3f7b988cd837b9b9ca86e8aac631ee538a1e
Manoj Ravichandran <themanojravichandran@outlook.com>
2025-06-26 23:28:38 +0530

    Update and rename service_1 to main.go

---
+++ main.go
@@ -0,0 +1,18 @@
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Welcome to Service 2!"
+@app.route('/hello')
+def hello():
+    return jsonify({"message": "Hello from Service 2"})
+
+@app.route('/ping')
+def ping():
+    return jsonify({"service": "2", "status": "ok"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8002)
